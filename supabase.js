2c0ad4c7dabcb1fd7188009909c168ee025cbb56
skul-hub <yoelafrido42@gmail.com>
2025-09-06 12:06:47 +0700

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,5 @@
+// supabase.js
+const SUPABASE_URL = "https://YOUR_PROJECT_ID.supabase.co";
+const SUPABASE_ANON_KEY = "YOUR_ANON_KEY";
+
+const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
