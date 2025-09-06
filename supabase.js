9f2d65c87025268deea334d0bab937a6beef2c07
skul-hub <yoelafrido42@gmail.com>
2025-09-06 12:20:51 +0700

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,5 +1,5 @@
 // supabase.js
-const SUPABASE_URL = "https://YOUR_PROJECT_ID.supabase.co";
-const SUPABASE_ANON_KEY = "YOUR_ANON_KEY";
+const SUPABASE_URL = "https://dvnfqoektrgarlplkkaj.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR2bmZxb2VrdHJnYXJscGxra2FqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTcxMDI2MzksImV4cCI6MjA3MjY3ODYzOX0.c_T4297HqKGo3KZYvWM6qXkiw4saE548Cp-UH7qZ7Ts";
 
 const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
